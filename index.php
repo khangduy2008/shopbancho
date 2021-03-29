3de4b7a1ff0e8d208a129b4617c6bc9f9be2434a
Your Name <you@example.com>
2021-03-29 19:13:45 +0700

    ssss

---
+++ index.php
@@ -0,0 +1,327 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web bán chó</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./style.css">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link href="https://pro.fontawesome.com/releases/v5.12.0/css/all.css" rel="stylesheet">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+      <link rel="stylesheet" href="./packet/sweetalert2.css">
+</head>
+<body>
+    <div class="nav1">
+        <img src="https://i.pets-health.com/pets-health-img/zhivotnie/986/sovet-1-kak-sshit-shapku-dlya-sobaki.jpg" alt="">
+        <ul>
+           <a href="../lesson4/index.html"><li>Home</li></a> 
+           <a href="https://www.youtube.com/"><li>Contact</li></a> 
+           <a href=""><li>Product</li></a> 
+           <a href=""><li>About us</li></a> 
+        </ul>
+    </div>
+
+    <div class="intro">
+        <h1 class="glow">Web bán chó của Duy</h1>
+    </div>
+
+
+
+
+
+
+    <!-- phần body các sản phẩm -->
+<div class="container">
+        <h1>Các sản phẩm hot</h1>
+    <div  class="container1">
+
+
+
+
+        <!-- Bắt đầu sản phẩm 1 -->
+        <div data-aos="fade-down" class="cart">
+            <img class="anh" src="https://azpet.com.vn/wp-content/uploads/2019/01/cho-poodle.jpg" alt="">
+            <p>Chó poodle</p>
+            <div class="footer-cart">
+                <p>700.500 đ</p>
+                <button data-toggle="modal" data-target="#myModal">Mua</button>
+            </div>
+        </div>
+        <!-- Bắt đầu sản phẩm 2 -->
+        <div data-aos="fade-down" class="cart">
+            <img class="anh" src="https://azpet.com.vn/wp-content/uploads/2019/10/cho-shiba-inu-1.jpg" alt="">
+            <p>Chó shiba </p>
+            <div class="footer-cart">
+                <p>1.000.000 đ</p>
+                <button data-toggle="modal" data-target="#myModal">mua</button>
+            </div>
+        </div>
+        <!-- Bắt đầu sản phẩm 3 -->
+        <div data-aos="fade-down" class="cart">
+            <img class="anh" src="https://azpet.com.vn/wp-content/uploads/2019/01/cho-phoc-soc.jpg" alt="">
+            <p>Chó phốc sóc</p>
+            <div class="footer-cart">
+                <p>1.000.000 đ</p>
+                <button data-toggle="modal" data-target="#myModal">mua</button>
+            </div>
+        </div>
+        <!-- Bắt đầu sản phẩm 4 -->
+        <div data-aos="fade-down" class="cart">
+            <img class="anh" src="https://azpet.com.vn/wp-content/uploads/2019/06/cho-alaska-malamute.jpg" alt="">
+            <p>Chó alaska</p>
+            <div class="footer-cart">
+                <p>1.500.000 đ</p>
+                <button data-toggle="modal" data-target="#myModal">mua</button>
+            </div>
+        </div>
+        <!-- Bắt đầu sản phẩm 5 -->
+        <div data-aos="fade-down" class="cart">
+            <img class="anh" src="https://azpet.com.vn/wp-content/uploads/2019/01/cho-husky.jpg" alt="">
+            <p>Chó husky</p>
+            <div class="footer-cart">
+                <p>1.000.000 đ</p>
+                <button data-toggle="modal" data-target="#myModal">mua</button>
+            </div>
+        </div>
+
+    </div>
+
+
+
+
+
+
+        <div  class="container1">
+        <!-- Bắt đầu sản phẩm 1 -->
+        <div data-aos="flip-left"class="cart">
+            <img class="anh" src="https://azpet.com.vn/wp-content/uploads/2019/01/cho-samoyed-trang-tuyet.jpg" alt="">
+            <p>Chó samoyed</p>
+            <div class="footer-cart">
+                <p>1.200.000 đ</p>
+                <button data-toggle="modal" data-target="#myModal">Mua</button>
+            </div>
+        </div>
+        <!-- Bắt đầu sản phẩm 2 -->
+        <div data-aos="flip-left"class="cart">
+            <img class="anh" src="https://azpet.com.vn/wp-content/uploads/2019/01/cho-corgi-7-e1560234421982.jpg" alt="">
+            <p>Chó corgi </p>
+            <div class="footer-cart">
+                <p>1.000.000 đ</p>
+                <button data-toggle="modal" data-target="#myModal">mua</button>
+            </div>
+        </div>
+        <!-- Bắt đầu sản phẩm 3 -->
+        <div data-aos="flip-left"class="cart">
+            <img class="anh" src="https://azpet.com.vn/wp-content/uploads/2019/01/cho-golden.jpg" alt="">
+            <p>Chó golden</p>
+            <div class="footer-cart">
+                <p>1.000.000 đ</p>
+                <button data-toggle="modal" data-target="#myModal">mua</button>
+            </div>
+        </div>
+        <!-- Bắt đầu sản phẩm 4 -->
+        <div data-aos="flip-left"class="cart">
+            <img class="anh" src="https://azpet.com.vn/wp-content/uploads/2019/01/cho-labrador.jpg" alt="">
+            <p>Chó labrador</p>
+            <div class="footer-cart">
+                <p>1.500.000 đ</p>
+                <button data-toggle="modal" data-target="#myModal">mua</button>
+            </div>
+        </div>
+        <!-- Bắt đầu sản phẩm 5 -->
+        <div data-aos="flip-left"class="cart">
+            <img class="anh" src="https://azpet.com.vn/wp-content/uploads/2019/04/cho-bull-anh.jpg" alt="">
+            <p>Chó husky</p>
+            <div class="footer-cart">
+                <p>1.000.000 đ</p>
+                <button data-toggle="modal" data-target="#myModal">mua</button>
+            </div>
+        </div>
+
+    </div>
+
+
+
+
+
+
+
+        <div data-aos="fade-right"class="container1">
+        <!-- Bắt đầu sản phẩm 1 -->
+        <div data-aos="fade-right"class="cart">
+            <img class="anh" src="https://azpet.com.vn/wp-content/uploads/2019/01/cho-bec-bi-malinois.jpg" alt="">
+            <p>Chó béc bỉ</p>
+            <div class="footer-cart">
+                <p>700.500 đ</p>
+                <button data-toggle="modal" data-target="#myModal">Mua</button>
+            </div>
+        </div>
+        <!-- Bắt đầu sản phẩm 2 -->
+        <div data-aos="fade-right"class="cart">
+            <img class="anh" src="https://azpet.com.vn/wp-content/uploads/2019/07/cho-beagle.jpg" alt="">
+            <p>Chó beagle </p>
+            <div class="footer-cart">
+                <p>1.000.000 đ</p>
+                <button data-toggle="modal" data-target="#myModal">mua</button>
+            </div>
+        </div>
+        <!-- Bắt đầu sản phẩm 3 -->
+        <div data-aos="fade-right"class="cart">
+            <img class="anh" src="https://azpet.com.vn/wp-content/uploads/2019/07/cho-border-collie-5.jpg" alt="">
+            <p>Chó border</p>
+            <div class="footer-cart">
+                <p>1.000.000 đ</p>
+                <button data-toggle="modal" data-target="#myModal">mua</button>
+            </div>
+        </div>
+        <!-- Bắt đầu sản phẩm 4 -->
+        <div data-aos="fade-right"class="cart">
+            <img class="anh" src="https://azpet.com.vn/wp-content/uploads/2019/07/cho-doberman.jpg" alt="">
+            <p>Chó doberman</p>
+            <div class="footer-cart">
+                <p>1.500.000 đ</p>
+                <button data-toggle="modal" data-target="#myModal">mua</button>
+            </div>
+        </div>
+        <!-- Bắt đầu sản phẩm 5 -->
+        <div data-aos="fade-right"class="cart">
+            <img class="anh" src="https://azpet.com.vn/wp-content/uploads/2019/01/cho-pug-mat-xe.jpg" alt="">
+            <p>Chó pug</p>
+            <div class="footer-cart">
+                <p>1.000.000 đ</p>
+                <button data-toggle="modal" data-target="#myModal">mua</button>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+<!-- Modal -->
+<div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Modal Header</h4>
+      </div>
+      <div class="modal-body">
+                <form id="contact" action="">
+            <div class="form-group">
+                <label for="pwd">Name:</label>
+                <input type="text" class="form-control" id="name">
+              </div>
+              <div class="form-group">
+                <label for="pwd">Message:</label>
+                <input type="text" class="form-control" id="message">
+              </div>
+            <div class="form-group">
+              <label for="email">Email:</label>
+              <input type="email" class="form-control" id="email">
+            </div>
+            <div class="form-group">
+                <label for="email">Phone:</label>
+                <input type="number" class="form-control" id="phone">
+              </div>
+
+
+            <button type="submit" class="btn btn-default">Submit</button>
+          </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
+<!--footer-->
+<div class="footer">
+    <div class="footer-top">
+        <div class="left">
+            <p>liên hệ:</p>
+            <p>Trần Khang Duy</p>
+            <a href="https://mail.google.com/mail/u/0/#inbox"><img src="./img/icons8-gmail-100.png"></a>
+            </div>
+
+        <div class="mid">
+            <img src="https://dewey.tailorbrands.com/production/brand_version_mockup_image/854/4912561854_3a0836be-cc49-4fe6-af92-4decd255a53e.png?cb=1616414105">
+        </div>
+
+        <div class="right">
+            <p>Địa chỉ:</p>
+            <p>Hỏi làm Gì . ko cho biết đâu</p>
+        </div>
+        
+    
+</div>
+
+
+
+
+
+         <script src="./packet/sweetalert2.all.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="./packet/confetti.js"></script>
+   <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+   <script>
+  AOS.init();
+   function sweetAlert(icon, content) {
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                onOpen: (toast) => {
+                    toast.addEventListener('mouseenter', Swal.stopTimer)
+                    toast.addEventListener('mouseleave', Swal.resumeTimer)
+                }
+            })
+
+            Toast.fire({
+                icon: icon,
+                title: content
+            })
+        }
+        $('#contact').on('submit', function (e) {
+               e.preventDefault()
+   
+               var name = $("#name");
+               var email = $("#email");
+               var phone = $("#phone");
+               var message = $("#message");
+               $.ajax({
+                   url: './sendEmail.php',
+                   method: 'POST',
+                   dataType: 'json',
+                   data: {
+                       name: name.val(),
+                       email: email.val(),
+                       message: message.val(),
+                       phone: phone.val(),
+                   },
+                   success: function (data) {
+                       sweetAlert(data.status,data.response);
+                       confetti.start()
+            setTimeout(function () {
+                confetti.stop();
+            }, 3000);
+                   }
+               });
+               return false;
+   
+           })
+</script> 
+</body>
+</html>
